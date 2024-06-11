99e534f6f8fcf903636bc068e02c24daf43bfc0a
Ronnfaed <prinzantiochus@gmail.com>
2024-06-11 17:44:20 -0300

    firebase query

--- src/pages/AgendamentoConsultas.js
+++ src/pages/AgendamentoConsultas.js
@@ -1,19 +1,131 @@
 import './AgendamentoConsultas.css'; // CSS da página
-import React, { useState } from 'react'; // Manipulação de estados lógicos
+import React, { useEffect ,useState } from 'react'; // Manipulação de estados lógicos
 import { Formik, Form, Field, ErrorMessage } from 'formik'; // Formulários
-import InputMask from 'react-input-mask'; // Mascaramento de dados
 import { db } from '../firebaseConfig'; // Firebase Sync
 import { collection, addDoc, query, where, getDocs } from "firebase/firestore";  // Firebase CRUD
+import InputMask from 'react-input-mask';
+
 
 const AgendamentoConsultas = () => {
+
+    // Métodos e funções para validar o CPF inserido
+
+    const [cpf, setCpf] = useState(''); // Método que define o valor do CPF, baseado no Input do usuário.
+    const [result, setResult] = useState(''); // Método que define e informa o resultado da verificação de cpf, se é válido ou inválido.
+
+    const handleInputChange = (e) => {
+        setCpf(e.target.value);
+    };
+  
+
+    const validateCPF = value => {
+        const cpf = value.replace(/[^\d]+/g, '');
+        if (cpf === '' || cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) return false;
+        let add = 0;
+        for (let i = 0; i < 9; i++) add += parseInt(cpf.charAt(i)) * (10 - i);
+        let rev = 11 - (add % 11);
+        if (rev === 10 || rev === 11) rev = 0;
+        if (rev !== parseInt(cpf.charAt(9))) return false;
+        add = 0;
+        for (let i = 0; i < 10; i++) add += parseInt(cpf.charAt(i)) * (11 - i);
+        rev = 11 - (add % 11);
+        if (rev === 10 || rev === 11) rev = 0;
+        if (rev !== parseInt(cpf.charAt(10))) return false;
+        return true;
+    };
+
+    // Não estamos mais utilizando o evento de lidar com o envio pois removemos o botão de enviar.
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      if (validateCPF(cpf)) {
+        setResult('CPF válido!');
+      } else {
+        setResult('CPF inválido!');
+      }
+    };
+
+    // Métodos e funções para a query no firebase
+
+
+    const [userData, setUserData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+  
+    useEffect(() => {
+      const fetchUserData = async () => {
+        try {
+          const q = query(collection(db, "pacientes"), where("cpf", "==", cpf));
+          const querySnapshot = await getDocs(q);
+  
+          if (!querySnapshot.empty) {
+            setUserData(querySnapshot.docs[0].data());
+          } else {
+            setUserData('');
+            setError("No user data found");
+          }
+        } catch (error) {
+          console.error("Error fetching document: ", error);
+          setError("Error fetching document");
+        } finally {
+          setLoading(false);
+        }
+      };
+  
+      if (cpf) {
+        fetchUserData();
+      }
+    }, [cpf]);
+  
+    const handleChange = (e) => 
+    {
+
+        const value = e.target.value;
+        setCpf(value);
+
+        if (value.length === 14) { // 14 caracteres inclui os pontos e o hífen da máscara
+
+          setResult(validateCPF(value) ? 'CPF válido!' : 'CPF inválido!');
+
+        } 
+        
+        else 
+
+        {
+          setResult('');
+        }
+
+    };
+
+
+
+
+    // Estrutura da página
+
     return (
         <div className = "main">
 
-        <div className = "title"><h1>Etapa 1</h1></div>
-
-        <div className = "idPaciente">
-        <input placeholder = "CPF do Paciente" required></input><button className = "btn" placeholder = "Teste" type = "submit"></button>
+        <div className = "title">
+            <h1>Agendamento de Consultas</h1>
+            <h2>Etapa 1</h2>
+            <p>Localize o cliente através do CPF e verifique se os dados estão corretos.</p>
         </div>
+
+
+        <div className="idPaciente">
+      <form>
+        <InputMask
+          name="cpf"
+          placeholder="CPF do Cliente"
+          type="text"
+          value={cpf}
+          onChange={handleChange}
+          mask="999.999.999-99"
+          required
+        />
+      </form>
+      <p className={result === 'CPF válido!' ? 'valid' : 'invalid'}>{result}</p>
+    </div>
+
 
         <Formik>
 
@@ -24,27 +136,60 @@ const AgendamentoConsultas = () => {
                             <div className="form-body">
                                 <div className="form-group">
                                     <label>Nome civil</label>
-                                    <Field type="text" name="nome" readOnly/>
+                                    <Field 
+                                     type="text"
+                                     name="nome"
+                                     onChange = {handleInputChange}
+                                     value = {userData ? userData.nome : ''}
+                                     readOnly
+                                     />
                                 </div>
                                 <div className="form-group">
                                     <label>Sexo</label>
-                                    <Field name="sexo" readOnly/>
+                                    <Field 
+                                    name="sexo"
+                                    onChange = {handleInputChange}
+                                    value = {userData ? userData.sexo : ''}
+                                    readOnly
+                                    />
                                 </div>
                                 <div className="form-group">
                                     <label>Nome social</label>
-                                    <Field type="text" name="nomeSocial" readOnly/>
+                                    <Field 
+                                    type="text"
+                                    name="nomeSocial"
+                                    onChange = {handleInputChange}
+                                    value = {userData ? userData.nomeSocial : ''}
+                                    readOnly
+                                    />
                                 </div>
                                 <div className="form-group">
                                     <label>Raça</label>
-                                    <Field name="raca" readOnly/>
+                                    <Field 
+                                    name="raca"
+                                    onChange = {handleInputChange}
+                                    value = {userData ? userData.raca : ''} 
+                                    readOnly
+                                    />
                                 </div>
                                 <div className="form-group">
                                     <label>CPF</label>
-                                    <Field name="cpf" readOnly/>
+                                    <Field 
+                                    name="cpf"
+                                    onChange = {handleInputChange}
+                                    value = {userData ? userData.cpf : ''} 
+                                    readOnly
+                                    />
                                 </div>
                                 <div className="form-group">
                                     <label>Profissão</label>
-                                    <Field type="text" name="profissao" readOnly/>
+                                    <Field 
+                                    type="text" 
+                                    name="profissao"
+                                    onChange = {handleInputChange}
+                                    value = {userData ? userData.profissao : ''}
+                                    readOnly
+                                    />
                                 </div>
                             </div>
                         </div>
@@ -53,11 +198,22 @@ const AgendamentoConsultas = () => {
                             <div className="form-body">
                                 <div className="form-group">
                                     <label>Email</label>
-                                    <Field type="email" name="email" readOnly/>
+                                    <Field 
+                                    type="email" 
+                                    name="email"
+                                    onChange = {handleInputChange}
+                                    value = {userData ? userData.email : ''}
+                                    readOnly
+                                    />
                                 </div>
                                 <div className="form-group">
                                     <label>Celular</label>
-                                    <Field name="celular" readOnly/>
+                                    <Field 
+                                    name="celular"
+                                    onChange = {handleInputChange}
+                                    value = {userData ? userData.celular : ''} 
+                                    readOnly
+                                    />
                                 </div>
                             </div>
                         </div>
@@ -67,7 +223,10 @@ const AgendamentoConsultas = () => {
             </Formik>
 
 
-            <div className = "title" style = {{ marginTop: '50px' }}><h1>Etapa 2</h1></div>
+            <div className = "title" style = {{ marginTop: '50px' }}>
+            <h2>Etapa 2</h2>
+            <p>Preencha o formulário abaixo e oficialize o agendamento clicando em confirmar</p>
+            </div>
       
     
         </div>
