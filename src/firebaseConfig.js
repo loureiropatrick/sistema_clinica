e1a1656551c53a644b18d97fda987c06eac81ba5
Luan <luan.bernardo11@gmail.com>
2024-06-10 21:00:52 -0300

    Atualização da page Cadastro Paciente e integração das collections do firebase

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyB29ilYX138LLrwI84HsHQkoO8GJD05WeU",
   authDomain: "maissaude-c3ca5.firebaseapp.com",
@@ -16,8 +12,8 @@ const firebaseConfig = {
   measurementId: "G-7Y497D3HG9"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-export {auth};
+export { auth, db };
